926346313ec413e7e5254744b34d590794bf0862
Steven Gadman <sgadman007@gmail.com>
2025-12-05 13:51:43 -0500

    Implement dashboard and invoices features with CRUD actions
    Added dashboard overview, invoices CRUD pages, and error handling. Integrated server actions for creating, updating, and deleting invoices. Refactored UI components to fetch data asynchronously and use suspense. Enhanced search and pagination for invoices. Added ESLint configuration and scripts. Removed obsolete files and updated dependencies.

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,13 +1,6 @@
 import postgres from 'postgres';
-import {
-  CustomerField,
-  CustomersTableType,
-  InvoiceForm,
-  InvoicesTable,
-  LatestInvoiceRaw,
-  Revenue,
-} from './definitions';
-import { formatCurrency } from './utils';
+import {CustomerField, CustomersTableType, InvoiceForm, InvoicesTable, LatestInvoiceRaw, Revenue,} from './definitions';
+import {formatCurrency} from './utils';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
@@ -16,14 +9,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    const data = await sql<Revenue[]>`SELECT * FROM revenue`;
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     // console.log('Data fetch completed after 3 seconds.');
 
-    return data;
+    return await sql<Revenue[]>`SELECT * FROM revenue`;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
@@ -39,11 +30,10 @@ export async function fetchLatestInvoices() {
       ORDER BY invoices.date DESC
       LIMIT 5`;
 
-    const latestInvoices = data.map((invoice) => ({
+    return data.map((invoice) => ({
       ...invoice,
       amount: formatCurrency(invoice.amount),
     }));
-    return latestInvoices;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch the latest invoices.');
